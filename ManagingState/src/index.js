edb3a30e575119eadd84e9675a6557925be0780a
马江林 <majianglin@totemtec.com>
2024-07-31 16:52:22 +0800

    update

--- ManagingState/src/index.js
+++ ManagingState/src/index.js
@@ -1,8 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-// import App from "./ReducerAndContext/App";
-import App from "./useReducer/App";
+import App from "./ReducerAndContext/App";
+// import App from "./useReducer/App";
+// import App from "./useContext/App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
